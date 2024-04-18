8a0ce23083c9351e46485eed86363c25de68295a
Ghostachio <mansour.hamza2.0@gmail.com>
2024-04-18 14:42:49 +0100

    started on the atlas webpage

---
+++ script.js
@@ -0,0 +1,35 @@
+const aboutButton = document.querySelector("#about-us-button");
+const contactButton = document.querySelector("#contact-us-button");
+const aboutSection = document.querySelector("#about-us-section");
+
+const aboutFooter = document.querySelector("#about-us-footer");
+const contactFooter = document.querySelector("#contact-us-footer");
+
+const contactSection = document.querySelector("#contact-us-section");
+
+const companyYear = document.querySelector("#date");
+const currentDate = new Date();
+
+const getCurrentYear = () => {
+  return currentDate.getFullYear();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  companyYear.innerHTML = `<p> Atlas Services ©${getCurrentYear()}</p>`;
+});
+
+aboutButton.addEventListener("click", function () {
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+});
+
+aboutFooter.addEventListener("click", function () {
+  aboutSection.scrollIntoView({ behavior: "smooth" });
+});
+
+contactButton.addEventListener("click", function () {
+  contactSection.scrollIntoView({ behavior: "smooth" });
+});
+
+contactFooter.addEventListener("click", function () {
+  contactSection.scrollIntoView({ behavior: "smooth" });
+});
